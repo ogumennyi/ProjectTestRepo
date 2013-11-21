0c4d16d6a802b4429faaa603d21dd80ebc2f36a2
IvanGromskyy <igromskyy@gmail.com>
2013-11-21 22:23:06 +0200

    add model Gameparties

---
+++ MoySport/src/com/moysport/model/Gameparties.java
@@ -0,0 +1,80 @@
+package com.moysport.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "GAMEPARTIES")
+public class Gameparties {
+	@Id
+	@Column(name = "idgp")
+	@GeneratedValue
+	private Integer idgp;
+
+	@Column(name = "IDGAME")
+	private Integer idgame;
+
+	@Column(name = "IDUSER")
+	private Integer iduser;
+
+	@Column(name = "VISITED")
+	private Integer visited;
+
+	@Column(name = "MARK")
+	private Integer mark;
+
+	@Column(name = "IDTEAM")
+	private Integer idteam;
+
+	public Integer getIdgp() {
+		return idgp;
+	}
+
+	public void setIdgp(Integer idgp) {
+		this.idgp = idgp;
+	}
+
+	public Integer getIdgame() {
+		return idgame;
+	}
+
+	public void setIdgame(Integer idgame) {
+		this.idgame = idgame;
+	}
+
+	public Integer getIduser() {
+		return iduser;
+	}
+
+	public void setIduser(Integer iduser) {
+		this.iduser = iduser;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getMark() {
+		return mark;
+	}
+
+	public void setMark(Integer mark) {
+		this.mark = mark;
+	}
+
+	public Integer getIdteam() {
+		return idteam;
+	}
+
+	public void setIdteam(Integer idteam) {
+		this.idteam = idteam;
+	}
+
+}
