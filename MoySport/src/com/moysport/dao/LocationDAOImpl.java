0cc429637dcf1aaee031372c5299dda92937143d
IvanGromskyy <igromskyy@gmail.com>
2014-02-02 16:35:47 +0200

    fix bugs

--- MoySport/src/com/moysport/dao/LocationDAOImpl.java
+++ MoySport/src/com/moysport/dao/LocationDAOImpl.java
@@ -7,7 +7,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.moysport.model.Event;
 import com.moysport.model.Location;
 
 @Repository
@@ -43,46 +42,25 @@ public class LocationDAOImpl implements LocationDAO {
 	public void updateLocation(Location location) {
 		System.out.println(">>>>"+location.getIdlocation());
 		System.out.println(">>>>"+location.getName());
-		// Retrieve existing event via id
+		// Retrieve existing location via id
 		Location existingLocation = (Location) sessionFactory.getCurrentSession().get(Location.class, location.getIdlocation());
-		// Assign updated values to this event
-		if (location.getName() != null)
-			existingLocation.setName(location.getName());
-		if (location.getCountry() != null)
-			existingLocation.setCountry(location.getCountry());
-		if (location.getRegion() != null)
-			existingLocation.setRegion(location.getRegion());
-		if (location.getCity() != null)
-			existingLocation.setCity(location.getCity());
-		if (location.getDistrict() != null)
-			existingLocation.setDistrict(location.getDistrict());
-		if (location.getStreet() != null)
-			existingLocation.setStreet(location.getStreet());
-		if (location.getBuilding() != null)
-			existingLocation.setBuilding(location.getBuilding());
-		if (location.getZip() != null)
-			existingLocation.setZip(location.getZip());
-		if (location.getDescription() != null)
-			existingLocation.setDescription(location.getDescription());
-		if (location.getContacts() != null)
-			existingLocation.setContacts(location.getContacts());
-		if (location.getHttplink() != null)
-			existingLocation.setHttplink(location.getHttplink());
-		if (location.getStatus() != null)
-			existingLocation.setStatus(location.getStatus());
+		// Assign updated values to this location
+		existingLocation.setName(location.getName());
+		existingLocation.setCountry(location.getCountry());
+		existingLocation.setRegion(location.getRegion());
+		existingLocation.setCity(location.getCity());
+		existingLocation.setDistrict(location.getDistrict());
+		existingLocation.setStreet(location.getStreet());
+		existingLocation.setBuilding(location.getBuilding());
+		existingLocation.setZip(location.getZip());
+		existingLocation.setDescription(location.getDescription());
+		existingLocation.setContacts(location.getContacts());
+		existingLocation.setHttplink(location.getHttplink());
+		existingLocation.setStatus(location.getStatus());
 		
-		/*if (event.getChangedate() != null)
-			existingEvent.setChangedate(event.getChangedate());
-		if (event.getLocations() != null)
-			existingEvent.setLocations(event.getLocations());
-		if (event.getMark() != null)
-			existingEvent.setMark(event.getMark());
-		if (event.getName() != null)
-			existingEvent.setName(event.getName());
-		if (event.getSport() != null)
-			existingEvent.setSport(event.getSport());
-		if (event.getMarkcnt() != null)
-			existingEvent.setMarkcnt(event.getMarkcnt());*/
+		existingLocation.setChangedate(location.getChangedate());
+		existingLocation.setIdchangeby(location.getIdchangeby());
+		
 		// Save updates
 		sessionFactory.getCurrentSession().save(existingLocation);
 	}
